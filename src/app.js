06db95cfc9e74b930adbcd96f0d3587dcfea75a9
Gael Valles <yanezgael576@gmail.com>
2025-11-13 00:49:54 -0600

    historial de citar y arreglos menores(1)

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5174',
     credentials: true
 }));
 app.use(express.json());
